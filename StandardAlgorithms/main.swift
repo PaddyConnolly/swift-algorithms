f44d83d9c952cd25cfdc6d78c0187398eb86d509
Patrick Connolly <connolly.p@etoncollege.org.uk>
2019-11-05 12:18:01 +0000

    Initial commit of project files

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Connolly, Patrick (IRG) on 05/11/2019.
+//  Copyright © 2019 Connolly, Patrick (IRG). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Patrick Connolly"
+
+for i in 1...3 {
+    print(i)
+}
+
+print(name)
